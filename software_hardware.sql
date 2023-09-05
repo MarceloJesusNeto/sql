22bd35713b2256d37e7172aaece6a92f3f0fd3d7
Aluno Tarde <marcelo.j.p.s.neto@gmail.com>
2023-09-05 17:31:08 -0300

    software_hardware

---
+++ software_hardware.sql
@@ -0,0 +1,9 @@
+/* 1- o software e um conjunto de componentes logicos de um sistema é algo não fisico em que você não pode o tocar como um programa.
+	
+	
+   2- o hardware diferente do software e um componente fisico como de um computador :monitor, teclado, mouse.
+	
+    
+   3- programa: e a execusão de codigos que descrevem oque deve ser feito pelo computador como redirecionmento ou mnipulação de dados. 
+	  codigo fonte: e um conjunto de palavras e simbolos mais conhecido como uma linguagem de programação	
+*/
